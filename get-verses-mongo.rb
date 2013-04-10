f555c934e4947106817df489302dc9d89b2c9ed8
Calvin Freitas <calvin.freitas@gmail.com>
2013-04-09 21:41:59 -0700

    create script to get verses from file and put in MongoDB

---
+++ get-verses-mongo.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+
+# using bundler http://gembundler.com/
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default)
+
+# other includes
+require './api-key.rb'		    # BIBLE_KEY
+require './models/Pack.rb'	    # Pack model
+require './BibleApi.rb'	    # Bible API
+
+options = {}
+
+optparse = OptionParser.new do |opts|
+    opts.banner = "Usage: $0 [options] file"
+
+    options[:name] = nil
+    opts.on( '-n', '--name NAME', 'Required: set the pack name' ) do |name|
+        options[:name] = name || "nothing"
+    end
+
+    opts.on( '-h', '--help', 'Display help screen' ) do
+        puts opts
+        exit
+    end
+end
+
+optparse.parse!
+
+if options[:name].nil?
+    puts "Required: --name NAME\n"
+    exit
+end
+
+# Setup BibleApi to take options for Mongo connection
+bibleApiOpts = {
+    :useMongo => true
+}
+bibleApi = BibleApi.new(bibleApiOpts)
+
+if(ARGV.size < 1) 
+    print "Please pass in a file\n"
+    exit
+end
+
+if(ARGV.size > 1)
+    print "Please send in one file at a time\n"
+    exit
+end
+
+File.open(ARGV[0]) do |file| 
+    pack = Pack.new(options[:name])
+    pack.verses = file.readlines
+
+    bibleApi.get_pack_data(pack)
+end
