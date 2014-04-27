5f743b107424663886c5c501be176561c269d266
Calvin Freitas <calvin.freitas@gmail.com>
2014-04-27 01:40:11 -0700

    remove unused method from pack model

--- models/Pack.rb
+++ models/Pack.rb
@@ -3,38 +3,19 @@ class Pack
     attr_accessor :verses
 
     def initialize(title = 'pack')
-	# initialize the pack title (e.g. 'a')
-	@title = title
+        # initialize the pack title (e.g. 'a')
+        @title = title
     end
 
     def get_title
         return @title
     end
 
-    def get_pack_data(pack)
-	bibleSearch = BibleSearch.new()
-	versesNeeded = self.check_for_verses(pack)
-
-	if versesNeeded > 0
-	    puts "Verses needed for #{pack.get_title}"
-	    url       = bibleSearch.get_search_url(pack.verses)
-	    data      = bibleSearch.get_search_result(url)
-	    @passages = self.get_passages(data)
-
-	    # print passages
-	    @passages.each_entry do |passage|
-		verse = self.distill(passage)
-	    end
-	else
-	    puts "Pack #{pack.get_title}: all verses found in MongoDB"
-	end
-    end
-
     def to_s
-	if(defined? @verses)
-	    return @title + ': ' + @verses.join(', ')
-	else
-	    return @title
-	end
+        if(defined? @verses)
+            return @title + ': ' + @verses.join(', ')
+        else
+            return @title
+        end
     end
 end
